86086a069e5d084396dba20359da15aeba1d2e39
Björn Ternes <bjoern.ternes@gmail.com>
2016-10-15 19:35:44 +0200

    update

--- js/controller.js
+++ js/controller.js
@@ -9,6 +9,9 @@ myApp.controller("LoginController", function ($scope, localStorageService) {
 });
 
 myApp.controller("QuestionsController", function ($scope, localStorageService) {
+    var index = [];
+    var questions = ["Magst du Flüchtlinge?", "Hast du ein Problem mit gleichgeschlechtlicher Ehe?", "Hast du das Gefühl, dass Deutschland immer mehr islamisiert?", "Denkst du, dass die Flüchtlinge uns die Arbeitsplätze wegnehmen?", "Die Ausländer haben kein Interesse sich anzupassen", "Würdest du dir Zeit nehmen Flüchtlingsheime zu bauen?", "Schwarze Menschen sind krimineller.", "Frauen können kein Auto fahren.", "Frauen haben keine handwerklichen Fähigkeiten", "Frauen können keine Landkarten lesen.", "Frauen können rechts von links nicht unterscheiden.", "Männer denken immer nur an das eine.", "Bist du der Meinung dass Frauen weniger Geld bekommen sollen.", "Ausländer liegen uns auf der Tasche", "Findest du das Frauen immer  Fett sind?", "Russen trinken immer Vodka.", "Asiaten schreiben immer gute Noten und sind fleißig.", "Bist du offen für Verbesserungen im Bereich Flüchtlinge?", "Zuwanderer gefährden unseren Sozialstaat"];
+
     $scope.quest = "test";
     alert(localStorageService.get("nick"));
 });
