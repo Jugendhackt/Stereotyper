48b19df6f5e058f07a71add918b7798b236cd95f
romankobosil <romankobosil@me.com>
2016-10-15 12:21:23 +0200

    Update

--- index.php
+++ index.php
@@ -10,7 +10,13 @@ $head->printHead();
 </br>
     <h1 class="no-margin-top">Stereotyper</h1>
 
+<p style="text-align: center; color: #868686; font-size: 15px; font-family: avenir; font-weight: 200;">&nbsp;</p> <p style="text-align: center;">Herzlich willkommen auf&nbsp;unserer Seite&nbsp;:)</p> <p style="text-align: center;">&nbsp;</p> <p style="text-align: center;">&nbsp;</p> <p style="text-align: center;">&nbsp;</p> <p>&nbsp;</p>
 
+<form action="/matcher" method="post">
+ <p>Nickname: <input type="text" class="form-control"  name="name" /></p>
+ <p>
+<button type="button" style="font-size: 40px;" class="btn btn-success">Go</button></p>
+</form>
 
 
     
